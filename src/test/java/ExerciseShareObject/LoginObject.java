b81ddc305265c474d5a0b6922fe8e59f1458445b
User <User@DESKTOP-B9T8IP8>
2021-06-10 12:53:01 +0700

    commit

--- src/test/java/ExerciseShareObject/LoginObject.java
+++ src/test/java/ExerciseShareObject/LoginObject.java
@@ -14,138 +14,150 @@ import ExerciseBaseCommon.BaseActions;
 import ExerciseBaseCommon.DataTest;
 
 public class LoginObject extends BaseActions {
-	//Include xpath
-	/*protected String passwd = "secret_sauce";
-	protected String url = "https://www.saucedemo.com/";
-	protected String login = "//input[@type='submit']";
-	protected String user = "//input[@name = 'user-name']";
-	protected String passbut = "//input[@id = 'password']";
-	protected String error = "//h3[@data-test='error']";
-	protected String errorbut = "//button[@class='error-button']";
-	protected String failure1 = "Epic sadface: Sorry, this user has been locked out.";
-	protected String failure2 = "Epic sadface: You can only access '/inventory.html' when you are logged in.";
-	protected String failure3 = "Epic sadface: Username is required";
-	protected String failure4 = "Epic sadface: Password is required";
-	protected String failure5 = "Epic sadface: Username and password do not match any user in this service";
-	//ExcelInit reader = new ExcelInit();
-*/	
-	//method or action into element on page
-	
-	public LoginObject OpenLoginPage (ExtentTest log, WebDriver driver) {
-		//GotoURL(log, driver, url);
+	// Include xpath
+	/*
+	 * protected String passwd = "secret_sauce"; protected String url =
+	 * "https://www.saucedemo.com/"; protected String login =
+	 * "//input[@type='submit']"; protected String user =
+	 * "//input[@name = 'user-name']"; protected String passbut =
+	 * "//input[@id = 'password']"; protected String error =
+	 * "//h3[@data-test='error']"; protected String errorbut =
+	 * "//button[@class='error-button']"; protected String failure1 =
+	 * "Epic sadface: Sorry, this user has been locked out."; protected String
+	 * failure2 =
+	 * "Epic sadface: You can only access '/inventory.html' when you are logged in."
+	 * ; protected String failure3 = "Epic sadface: Username is required"; protected
+	 * String failure4 = "Epic sadface: Password is required"; protected String
+	 * failure5 =
+	 * "Epic sadface: Username and password do not match any user in this service";
+	 * //ExcelInit reader = new ExcelInit();
+	 */
+	// method or action into element on page
+
+	public LoginObject OpenLoginPage(ExtentTest log, WebDriver driver) {
+		// GotoURL(log, driver, url);
 		GotoURL(log, driver, readExl("Login", DataTest.ColUrl, 1));
 		return this;
 	}
-	public LoginObject Login (ExtentTest log, WebDriver driver) {
-		/*GotoURL(log, driver, url);
-		InputdataXpath(log, driver, user, "standard_user");
-		InputdataXpath(log, driver, passbut, passwd);
-		ClickXpath(log, driver, login);*/
+
+	public LoginObject Login(ExtentTest log, WebDriver driver) {
+		/*
+		 * GotoURL(log, driver, url); InputdataXpath(log, driver, user,
+		 * "standard_user"); InputdataXpath(log, driver, passbut, passwd);
+		 * ClickXpath(log, driver, login);
+		 */
 		GotoURL(log, driver, readExl("Login", DataTest.ColUrl, 1));
 		InputdataXpath(log, driver, readExl("Xpath", DataTest.ColXpath, 1), readExl("Login", DataTest.ColUser, 1));
 		InputdataXpath(log, driver, readExl("Xpath", DataTest.ColXpath, 2), readExl("Login", DataTest.ColPass, 1));
 		ClickXpath(log, driver, readExl("Xpath", DataTest.ColXpath, 3));
 		return this;
-	} 
-	
-	//Input Data
-	public LoginObject InputUserName (ExtentTest log, WebDriver driver, String data) {
+	}
+
+	// Input Data
+	public LoginObject InputUserName(ExtentTest log, WebDriver driver, String data) {
 		InputdataXpath(log, driver, readExl("Xpath", DataTest.ColXpath, 1), data);
 		return this;
 	}
+
 	public LoginObject InputPasswd(ExtentTest log, WebDriver driver) {
 		InputdataXpath(log, driver, readExl("Xpath", DataTest.ColXpath, 2), readExl("Login", DataTest.ColPass, 1));
 		return this;
 	}
+
 	public LoginObject InputPasswd_error(ExtentTest log, WebDriver driver, String data) {
 		InputdataXpath(log, driver, readExl("Xpath", DataTest.ColXpath, DataTest.RowXpath_passwd), data);
 		return this;
 	}
-	
-	//Click
-	public LoginObject ClickLogin_but (ExtentTest log, WebDriver driver) {
+
+	// Click
+	public LoginObject ClickLogin_but(ExtentTest log, WebDriver driver) {
 		ClickXpath(log, driver, readExl("Xpath", DataTest.ColXpath, 3));
 		return this;
 	}
-	//Verify
-	public LoginObject Verify_fail (ExtentTest log,WebDriver driver, String expect) {
+
+	// Verify
+	public LoginObject Verify_fail(ExtentTest log, WebDriver driver, String expect) {
 		AssertEqual(log, driver, readExl("Xpath", DataTest.ColXpath, 4), expect);
 		return this;
 	}
-	
-	public LoginObject Verify_fail_1 (ExtentTest log,SoftAssert softAssert,WebDriver driver) {
-		sAssertEqual(log, softAssert, driver, readExl("Xpath", DataTest.ColXpath, 4), readExl("Login", DataTest.ColError, 1));
+
+	public LoginObject Verify_fail_1(ExtentTest log, SoftAssert softAssert, WebDriver driver) {
+		sAssertEqual(log, softAssert, driver, readExl("Xpath", DataTest.ColXpath, 4),
+				readExl("Login", DataTest.ColError, 1));
 		return this;
 	}
-	public LoginObject Verify_fail_2 (SoftAssert softAssert,WebDriverWait wait,ExtentTest log,WebDriver driver) {
+
+	public LoginObject Verify_fail_2(SoftAssert softAssert, WebDriverWait wait, ExtentTest log, WebDriver driver) {
 		waitclickable(wait, driver, readExl("Xpath", DataTest.ColXpath, 4));
-		GetTextEqual(softAssert,log, driver, readExl("Xpath", DataTest.ColXpath, 4), readExl("Login", DataTest.ColError, 2));
+		GetTextEqual(softAssert, log, driver, readExl("Xpath", DataTest.ColXpath, 4),
+				readExl("Login", DataTest.ColError, 2));
 		waitclickable(wait, driver, readExl("Xpath", DataTest.ColXpath, 5));
 		ClickXpath(log, driver, readExl("Xpath", DataTest.ColXpath, 5));
 		return this;
 	}
-	
-	public LoginObject Verify_fail_3 (SoftAssert softAssert,ExtentTest log,WebDriver driver) {
+
+	public LoginObject Verify_fail_3(SoftAssert softAssert, ExtentTest log, WebDriver driver) {
 		InputdataXpath(log, driver, readExl("Xpath", 1, 2), "aaa");
 		ClickXpath(log, driver, readExl("Xpath", 1, 3));
-		GetTextEqual(softAssert,log, driver, readExl("Xpath", 1, 4), readExl("Login", DataTest.ColError, 3));
+		GetTextEqual(softAssert, log, driver, readExl("Xpath", 1, 4), readExl("Login", DataTest.ColError, 3));
 		return this;
 	}
-	public LoginObject Verify_fail_4 (SoftAssert softAssert,WebDriverWait wait,ExtentTest log,WebDriver driver) {
+
+	public LoginObject Verify_fail_4(SoftAssert softAssert, WebDriverWait wait, ExtentTest log, WebDriver driver) {
 		waitvisible(wait, driver, readExl("Xpath", 1, 1));
 		InputdataXpath(log, driver, readExl("Xpath", 1, 1), "abc");
 		ClearDataxpath(log, driver, readExl("Xpath", 1, 2));
 		ClickXpath(log, driver, readExl("Xpath", 1, 3));
-		GetTextEqual(softAssert,log, driver, readExl("Xpath", 1, 4), readExl("Login", DataTest.ColError, 4));
+		GetTextEqual(softAssert, log, driver, readExl("Xpath", 1, 4), readExl("Login", DataTest.ColError, 4));
 		return this;
 	}
-	public LoginObject Verify_fail_5 (SoftAssert softAssert,ExtentTest log,WebDriver driver) {
+
+	public LoginObject Verify_fail_5(SoftAssert softAssert, ExtentTest log, WebDriver driver) {
 		ClearDataxpath(log, driver, readExl("Xpath", 1, 1));
 		InputdataXpath(log, driver, readExl("Xpath", 1, 1), "abc");
 		ClearDataxpath(log, driver, readExl("Xpath", 1, 2));
 		InputdataXpath(log, driver, readExl("Xpath", 1, 2), "aaa");
 		ClickXpath(log, driver, readExl("Xpath", 1, 3));
-		GetTextEqual(softAssert,log, driver, readExl("Xpath", 1, 4), readExl("Login", DataTest.ColError, 5));
+		GetTextEqual(softAssert, log, driver, readExl("Xpath", 1, 4), readExl("Login", DataTest.ColError, 5));
 		return this;
 	}
-	public LoginObject Compare_login_but (ExtentTest log,SoftAssert softAssert,WebDriver driver) {
+
+	public LoginObject Compare_login_but(ExtentTest log, SoftAssert softAssert, WebDriver driver) {
 		sAssertDisplay_true(log, softAssert, driver, readExl("Xpath", 1, 3));
 		return this;
 	}
-	
-	//Read excel
-	
-	
-	
-	//Track SoftAssert
-	public LoginObject TracksAssert (SoftAssert softAssert) {
+
+	// Read excel
+
+	// Track SoftAssert
+	public LoginObject TracksAssert(SoftAssert softAssert) {
 		softAssertAll(softAssert);
 		return this;
 	}
-	
-	public LoginObject LoginFail_1(ExtentReports report,ExtentTest log, WebDriver driver, Assert Assert) {
-		ArrayList<String> lstUser = lstData("LoginFail", 0); 
+
+	public LoginObject LoginFail_1(ExtentReports report, ExtentTest log, WebDriver driver) {
+		ArrayList<String> lstUser = lstData("LoginFail", 0);
 		ArrayList<String> lstPasswd = lstData("LoginFail", 1);
 		ArrayList<String> lstError = lstData("LoginFail", 2);
 		ArrayList<String> lstTCName = lstData("LoginFail", 3);
-		
-		for(int i = 1; i < lstUser.size(); i++) {
+
+		for (int i = 1; i < lstUser.size(); i++) {
 			String username = lstUser.get(i);
 			String password = lstPasswd.get(i);
 			String error = lstError.get(i);
 			String tcName = lstTCName.get(i);
-			log = report.createTest(tcName);
-			OpenLoginPage(log, driver);
-			InputUserName(log, driver, username);
-			InputPasswd_error(log, driver, password);
-			ClickLogin_but(log, driver);
-			//Verify_fail(log, Assert, driver, error);
-			
+			if (!tcName.isEmpty()) {
+				log = report.createTest(tcName);
+				OpenLoginPage(log, driver);
+				InputUserName(log, driver, username);
+				InputPasswd_error(log, driver, password);
+				ClickLogin_but(log, driver);
+				Verify_fail(log, driver, error);
+			}
 		}
-		
-		
+
 		return this;
-				
+
 	}
 
 }
--- src/test/java/ExercisemvnTestcase/Testcase_login.java
+++ src/test/java/ExercisemvnTestcase/Testcase_login.java
@@ -1,6 +1,5 @@
 package ExercisemvnTestcase;
 
-
 import java.util.ArrayList;
 
 import org.testng.annotations.Test;
@@ -8,147 +7,129 @@ import ExerciseBaseCommon.BaseTest;
 import ExerciseBaseCommon.DataTest;
 import ExerciseBaseCommon.ExcelInit;
 
+public class Testcase_login extends BaseTest { // use extends command to get value from a variable of another class
 
-public class Testcase_login extends BaseTest { //use extends command to get value from a variable of another class
-	
-	
-	
-	//@Test (priority = 1)
+	// @Test (priority = 1)
 	public void login_Standard() {
-		//create a report for test case
-		log = report.createTest("Login Standard"); //create a log in each test case it should be input in each test case with test case name in ()
-		//LoginObject lb = new LoginObject();
-		//InventoryObject ib = new InventoryObject();
+		// create a report for test case
+		log = report.createTest("Login Standard"); // create a log in each test case it should be input in each test
+													// case with test case name in ()
+		// LoginObject lb = new LoginObject();
+		// InventoryObject ib = new InventoryObject();
 		lb.OpenLoginPage(log, driver)
-		//.InputUserName(log, driver, "standard_user")
-		.InputUserName(log, driver, excel.readExl("Login", DataTest.ColUser, 1))
-		.InputPasswd(log, driver)
-		.ClickLogin_but(log, driver);
+				// .InputUserName(log, driver, "standard_user")
+				.InputUserName(log, driver, excel.readExl("Login", DataTest.ColUser, 1)).InputPasswd(log, driver)
+				.ClickLogin_but(log, driver);
 		sleep_n(2);
-		ib.CompareTitle(log, softAssert, driver, excel.readExl("Inventory", DataTest.ColPagetitle, 1))
-		.Logout(log, wait, driver);
+		ib.CompareTitle(log, softAssert, driver, excel.readExl("Inventory", DataTest.ColPagetitle, 1)).Logout(log, wait,
+				driver);
 		lb.TracksAssert(softAssert);
-		
-		
+
 	}
-	//@Test (priority = 2)
+
+	// @Test (priority = 2)
 	public void login_failure() {
 		log = report.createTest("Login Failure");
-		//LoginObject lb = new LoginObject();
-		lb.OpenLoginPage(log, driver)
-		.InputUserName(log, driver, excel.readExl("Login", DataTest.ColUser, 2))
-		.InputPasswd(log, driver)
-		.ClickLogin_but(log, driver);
+		// LoginObject lb = new LoginObject();
+		lb.OpenLoginPage(log, driver).InputUserName(log, driver, excel.readExl("Login", DataTest.ColUser, 2))
+				.InputPasswd(log, driver).ClickLogin_but(log, driver);
 		sleep_n(2);
-		lb.Verify_fail_1(log, softAssert, driver)
-		.TracksAssert(softAssert);
+		lb.Verify_fail_1(log, softAssert, driver).TracksAssert(softAssert);
 
-		/*mymethod.GotoURL(log,driver, url);
-		mymethod.InputdataXpath(log,driver, user,"locked_out_user");
-		mymethod.InputdataXpath(log,driver,passbut , passwd);
-		mymethod.ClickXpath(log,driver, login);
-		sleep_3();
-		mymethod.sAssertEqual(log,softAssert, driver, error, failure1);
-		sleep_3();
-		mymethod.softAssertAll(softAssert);*/
-		
+		/*
+		 * mymethod.GotoURL(log,driver, url); mymethod.InputdataXpath(log,driver,
+		 * user,"locked_out_user"); mymethod.InputdataXpath(log,driver,passbut ,
+		 * passwd); mymethod.ClickXpath(log,driver, login); sleep_3();
+		 * mymethod.sAssertEqual(log,softAssert, driver, error, failure1); sleep_3();
+		 * mymethod.softAssertAll(softAssert);
+		 */
+
 	}
-	//@Test (priority = 2)
+
+	// @Test (priority = 2)
 	public void login_problem() {
 		log = report.createTest("Login Problem");
-		//LoginObject lb = new LoginObject();
-		//InventoryObject ib = new InventoryObject();
-		lb.OpenLoginPage(log, driver)
-		.InputUserName(log, driver, excel.readExl("Login", DataTest.ColUser, 3))
-		.InputPasswd(log, driver)
-		.ClickLogin_but(log, driver);
+		// LoginObject lb = new LoginObject();
+		// InventoryObject ib = new InventoryObject();
+		lb.OpenLoginPage(log, driver).InputUserName(log, driver, excel.readExl("Login", DataTest.ColUser, 3))
+				.InputPasswd(log, driver).ClickLogin_but(log, driver);
 		sleep_n(2);
-		ib.Verify_Link_item_problem(softAssert, log, wait, driver)
-		.Logout(log, wait, driver);
+		ib.Verify_Link_item_problem(softAssert, log, wait, driver).Logout(log, wait, driver);
 		lb.TracksAssert(softAssert);
-		
-		/*mymethod.GotoURL(log,driver, url);
-		mymethod.InputdataXpath(log,driver, user,"problem_user");
-		mymethod.InputdataXpath(log,driver,passbut , passwd);
-		sleep_3();
-		mymethod.ClickXpath(log,driver, login);
-		mymethod.ClickFormat(log, driver, itemlink, "item_1_img_link");
-		sleep_3();	
-		mymethod.sAssertEqual(log,softAssert, driver, itemdetail_Title, "Sauce Labs Fleece Jacket");
-		logout();
-		mymethod.softAssertAll(softAssert);*/
+
+		/*
+		 * mymethod.GotoURL(log,driver, url); mymethod.InputdataXpath(log,driver,
+		 * user,"problem_user"); mymethod.InputdataXpath(log,driver,passbut , passwd);
+		 * sleep_3(); mymethod.ClickXpath(log,driver, login); mymethod.ClickFormat(log,
+		 * driver, itemlink, "item_1_img_link"); sleep_3();
+		 * mymethod.sAssertEqual(log,softAssert, driver, itemdetail_Title,
+		 * "Sauce Labs Fleece Jacket"); logout(); mymethod.softAssertAll(softAssert);
+		 */
 	}
-	//@Test (priority = 2)
-	/*public void login_performance() {
-		driver.get(url);
-		driver.findElement(By.xpath(user)).sendKeys("performance_glitch_user");
-		driver.findElement(By.xpath(passbut)).sendKeys(passwd);
-		driver.findElement(By.xpath(login)).click();
-		WebElement title = driver.findElement(By.xpath("//button[contains (@name,'backpack')]"));
-		Assert.assertFalse(title.getCssValue("font-size").equalsIgnoreCase("14px"));*/
-		/*sleep_20();
-		Assert.assertTrue(title.isDisplayed());
-		Assert.assertTrue(title.isDisplayed());
-	}*/
-	
-	
-	@Test (priority = 2)
+	// @Test (priority = 2)
+	/*
+	 * public void login_performance() { driver.get(url);
+	 * driver.findElement(By.xpath(user)).sendKeys("performance_glitch_user");
+	 * driver.findElement(By.xpath(passbut)).sendKeys(passwd);
+	 * driver.findElement(By.xpath(login)).click(); WebElement title =
+	 * driver.findElement(By.xpath("//button[contains (@name,'backpack')]"));
+	 * Assert.assertFalse(title.getCssValue("font-size").equalsIgnoreCase("14px"));
+	 */
+	/*
+	 * sleep_20(); Assert.assertTrue(title.isDisplayed());
+	 * Assert.assertTrue(title.isDisplayed()); }
+	 */
+
+	@Test(priority = 2)
 	public void login_error_page() {
-		ExcelInit reader = new ExcelInit();
-		
+		/*ExcelInit reader = new ExcelInit();
+
 		ArrayList<String> lstUser = reader.readExcelFileAtColumn(BaseTest.fileName, "LoginFail", 0);
 		ArrayList<String> lstPasswd = reader.readExcelFileAtColumn(BaseTest.fileName, "LoginFail", 1);
 		ArrayList<String> lstError = reader.readExcelFileAtColumn(BaseTest.fileName, "LoginFail", 2);
 		ArrayList<String> lstTCName = reader.readExcelFileAtColumn(BaseTest.fileName, "LoginFail", 3);
-		
-		for(int i = 1; i < lstUser.size(); i++) {
+
+		for (int i = 1; i < lstUser.size(); i++) {
 			String username = lstUser.get(i);
 			String password = lstPasswd.get(i);
 			String error = lstError.get(i);
 			String tcName = lstTCName.get(i);
-			log = report.createTest(tcName);
-			
-			lb.OpenLoginPage(log, driver)
-			.InputUserName(log, driver, username)
-			.InputPasswd_error(log, driver, password)
-			.ClickLogin_but(log, driver)
-			.Verify_fail(log, driver, error);
-			
-			
-		}
-		//lb.LoginFail_1(report, log, driver, Assert);
-		
-		
-		/*ib.OpenInvenPage(log, driver);
-		lb.Verify_fail_2(softAssert,wait, log, driver)
-		.OpenLoginPage(log, driver)
-		.Verify_fail_3(softAssert,log, driver)
-		.OpenLoginPage(log, driver)
-		.Verify_fail_4(softAssert,wait, log, driver)
-		.OpenLoginPage(log, driver)
-		.Verify_fail_5(softAssert,log, driver)
-		.TracksAssert(softAssert);*/
-		
-		/*mymethod.GotoURL(log, driver, urlogin);
-		mymethod.waitclickable(wait, driver, error);
-		mymethod.GetTextEqual(log, driver, error, failure2);
-		mymethod.waitclickable(wait, driver, errorbut);
-		mymethod.ClickXpath(log, driver, errorbut);
-		mymethod.waitvisible(wait, driver, user);
-		mymethod.InputdataXpath(log, driver, user, "abc");
-		mymethod.ClickXpath(log, driver, login);
-		
-		mymethod.GetTextEqual(log, driver, error, failure4);
-		mymethod.ClearDataxpath(log, driver, user);
-		mymethod.InputdataXpath(log, driver, passbut, "aaa");
-		mymethod.ClickXpath(log, driver, login);
-		mymethod.GetTextEqual(log, driver, error, failure3);
-		mymethod.ClearDataxpath(log, driver, passbut);
-		mymethod.InputdataXpath(log, driver, user, "abc");
-		mymethod.InputdataXpath(log, driver, passbut, "aaa");
-		mymethod.ClickXpath(log, driver, login);
-		mymethod.GetTextEqual(log, driver, error, failure5);
-		mymethod.ClearDataxpath(log, driver, user);
-		mymethod.ClearDataxpath(log, driver,passbut);*/
+			if (!tcName.isEmpty()) {
+				log = report.createTest(tcName);
+
+				lb.OpenLoginPage(log, driver).InputUserName(log, driver, username)
+						.InputPasswd_error(log, driver, password).ClickLogin_but(log, driver)
+						.Verify_fail(log, driver, error);
+
+			}
+		}*/
+		lb.LoginFail_1(report, log, driver);
+
+		/*
+		 * ib.OpenInvenPage(log, driver); lb.Verify_fail_2(softAssert,wait, log, driver)
+		 * .OpenLoginPage(log, driver) .Verify_fail_3(softAssert,log, driver)
+		 * .OpenLoginPage(log, driver) .Verify_fail_4(softAssert,wait, log, driver)
+		 * .OpenLoginPage(log, driver) .Verify_fail_5(softAssert,log, driver)
+		 * .TracksAssert(softAssert);
+		 */
+
+		/*
+		 * mymethod.GotoURL(log, driver, urlogin); mymethod.waitclickable(wait, driver,
+		 * error); mymethod.GetTextEqual(log, driver, error, failure2);
+		 * mymethod.waitclickable(wait, driver, errorbut); mymethod.ClickXpath(log,
+		 * driver, errorbut); mymethod.waitvisible(wait, driver, user);
+		 * mymethod.InputdataXpath(log, driver, user, "abc"); mymethod.ClickXpath(log,
+		 * driver, login);
+		 * 
+		 * mymethod.GetTextEqual(log, driver, error, failure4);
+		 * mymethod.ClearDataxpath(log, driver, user); mymethod.InputdataXpath(log,
+		 * driver, passbut, "aaa"); mymethod.ClickXpath(log, driver, login);
+		 * mymethod.GetTextEqual(log, driver, error, failure3);
+		 * mymethod.ClearDataxpath(log, driver, passbut); mymethod.InputdataXpath(log,
+		 * driver, user, "abc"); mymethod.InputdataXpath(log, driver, passbut, "aaa");
+		 * mymethod.ClickXpath(log, driver, login); mymethod.GetTextEqual(log, driver,
+		 * error, failure5); mymethod.ClearDataxpath(log, driver, user);
+		 * mymethod.ClearDataxpath(log, driver,passbut);
+		 */
 	}
 }
